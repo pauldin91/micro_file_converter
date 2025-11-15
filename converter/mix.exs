7ae0237cb2986bd5b66973622d2c2a214752c099
pauldin91 <pauldin91@gmail.com>
2025-11-15 17:12:49 +0200

    add dependencies

--- converter/mix.exs
+++ converter/mix.exs
@@ -59,7 +59,7 @@ defmodule Converter.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:amqp, "~> 3.3"}
+      {:amqp, "~> 4.0"}
     ]
   end
 
