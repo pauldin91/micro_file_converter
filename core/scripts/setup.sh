74aaa031894a84b3fcf35379c7cf1ac62a547145
pauldin91 <pauldin91@gmail.com>
2025-12-19 06:41:40 +0200

    move form to heex

--- core/scripts/setup.sh
+++ core/scripts/setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-mix phx.gen.auth
-mix phx.gen.live Uploads Batch batches status:string --binary-id
-mix phx.gen.live Items Picture pictures  batch_id:references:batch name:string transform:string size:integer --binary-id
+mix phx.gen.auth Accounts User users
+mix phx.gen.live Uploads Batch batches status:string user_id:references:user --binary-id
+mix phx.gen.context Items Picture pictures  batch_id:references:batch name:string transform:string size:integer --binary-id
