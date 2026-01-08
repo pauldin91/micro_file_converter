d3fcee7e54a6e9e48b71043f5681ea2a915195e1
pauldin91 <pauldin91@gmail.com>
2026-01-08 06:57:20 +0200

    test script

--- scripts/rabbit/publish.sh
+++ scripts/rabbit/publish.sh
@@ -2,12 +2,13 @@
 set -euo pipefail
 
 test_dir=4536e1fb-82c1-4fb2-a40e-5a46e4254496
+upload_dir=./uploads/$test_dir
+sample_dir=./scripts/rabbit/
 
-pwd
-ls uploads
-mkdir -p "./uploads/$test_dir"
-cp ./scripts/rabbit/*.png "./uploads/$test_dir/"
-cp ./scripts/rabbit/*.json "./uploads/$test_dir"
+mkdir -p "$upload_dir"
+
+cp "$sample_dir"/*.png "$upload_dir/"
+cp "$sample_dir"/*.json "$upload_dir"
 
 docker exec -i rabbit sh -c '
   rabbitmqadmin publish \
