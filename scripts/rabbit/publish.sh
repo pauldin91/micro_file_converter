c3742e6affd4a72360d0640b41ba62209599fd99
pauldin91 <pauldin91@gmail.com>
2026-01-08 06:37:10 +0200

    test script

--- scripts/rabbit/publish.sh
+++ scripts/rabbit/publish.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
+test_dir=4536e1fb-82c1-4fb2-a40e-5a46e4254496
+
+pwd
+ls uploads
+mkdir -p "./uploads/$test_dir"
+cp ./scripts/rabbit/*.png "./uploads/$test_dir/"
+cp ./scripts/rabbit/*.json "./uploads/$test_dir"
+
 docker exec -i rabbit sh -c '
   rabbitmqadmin publish \
     exchange=amq.default \
     routing_key="$1" \
     payload="$(cat)"
 ' sh "$1" < "$2"
+
+
 
