9650f5be0456d1ffbc49f1a669360bc8f8a91d40
pauldin91 <pauldin91@gmail.com>
2026-01-13 18:44:26 +0200

    split json to multiple messages

--- scripts/rabbit/publish.sh
+++ scripts/rabbit/publish.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 set -euo pipefail
+routing_key="$1"
+sample_dir="./scripts/rabbit"
 
-test_dir=4536e1fb-82c1-4fb2-a40e-5a46e4254496
-upload_dir=./uploads/$test_dir
-sample_dir=./scripts/rabbit/
+while IFS= read -r item; do
+  uuid=$(uuidgen)
+  echo "publishing id=$uuid"
+  upload_dir="./uploads/$uuid"
+  
+  rm -rf "$upload_dir"
+  mkdir -p "$upload_dir"
+  
+  cp "$sample_dir"/*.png "$upload_dir/"
+  echo "$item" > "$upload_dir/$uuid.json"
+  payload="$(echo "$item" | jq --arg id "$uuid" '. + {id: $id}')"
 
-rm -rf "$upload_dir"
-mkdir -p "$upload_dir"
-
-cp "$sample_dir"/*.png "$upload_dir/"
-cp "$sample_dir"/*.json "$upload_dir"
-
-
-jq -c '.[]' "$sample_dir"*.json | while read -r item; do
-  echo "$item" | docker exec -i rabbit sh -c '
-    rabbitmqadmin publish message \
-      --exchange=amq.default \
-      --routing-key="$1" \
-      --payload="$(cat)"
-  ' sh "$1"
-done
-
-
-
+  docker exec rabbit rabbitmqadmin publish \
+    exchange=amq.default \
+    routing_key="$routing_key" \
+    payload="$payload"
+done < <(jq -c '.[]' "$sample_dir"/*.json)
