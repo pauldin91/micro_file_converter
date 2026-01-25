ddc3fd0c92393c03059871c38a6eff06049fa132
pauldin91 <pauldin91@gmail.com>
2026-01-25 19:36:55 +0200

    impl simple publisherr

--- image_service/src/application/publisher.rs
+++ image_service/src/application/publisher.rs
@@ -1,6 +1,9 @@
-use lapin::{Connection, ConnectionProperties};
-
 use crate::domain::config;
+use lapin::{Connection, ConnectionProperties};
+use rabbitmq_stream_client::Environment;
+use rabbitmq_stream_client::error::StreamCreateError;
+use rabbitmq_stream_client::types::{ByteCapacity, Message, ResponseCode, StreamCreator};
+use tracing::info;
 
 pub struct RabbitMqPublisher {
     host: String,
@@ -14,9 +17,34 @@ impl RabbitMqPublisher {
         Self { host, queue }
     }
 
-    pub async fn publish(&self, msg: String) {
-        let conn_res = Connection::connect(&self.host, ConnectionProperties::default()).await;
+    pub async fn publish(&self, msg: String) -> Result<(), Box<dyn std::error::Error>> {
+        use rabbitmq_stream_client::Environment;
+        let environment = Environment::builder().host(&self.host).build().await?;
+        let stream = msg.as_str();
+        let create_response = environment
+            .stream_creator()
+            .max_length(ByteCapacity::GB(5))
+            .create(&self.queue)
+            .await;
 
-        todo!()
+        if let Err(e) = create_response {
+            if let StreamCreateError::Create { stream, status } = e {
+                match status {
+                    ResponseCode::StreamAlreadyExists => {}
+                    err => {
+                        println!("Error creating stream: {:?} {:?}", stream, err);
+                    }
+                }
+            }
+        }
+
+        let producer = environment.producer().build(stream).await?;
+
+        producer
+            .send_with_confirm(Message::builder().body(stream).build())
+            .await?;
+        info!("Sent message to stream: {}", stream);
+        producer.close().await?;
+        Ok(())
     }
 }
