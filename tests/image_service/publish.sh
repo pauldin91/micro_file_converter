4d633f677cb8b3f42bb8bb90da8bf1d4f9e85fd5
pauldin91 <pauldin91@gmail.com>
2026-02-18 07:56:45 +0200

    refactlor test script to curl

--- tests/image_service/publish.sh
+++ tests/image_service/publish.sh
@@ -16,8 +16,13 @@ while IFS= read -r item; do
   echo "$item" > "$upload_dir/$uuid.json"
   payload="$(echo "$item" | jq --arg id "$uuid" '. + {id: $id}+{timestamp: (now|todateiso8601)}')"
 
-  docker exec rabbit rabbitmqadmin publish \
-    exchange=amq.default \
-    routing_key="$routing_key" \
-    payload="$payload"
+  curl -s -u guest:guest \
+  -H "Content-Type: application/json" \
+  -X POST "http://localhost:15672/api/exchanges/%2F/amq.default/publish" \
+  -d "$(jq -n \
+    --arg rk "$routing_key" \
+    --argjson p "$payload" \
+    '{properties: {}, routing_key: $rk, payload: ($p | tojson), payload_encoding: "string"}')"
+
 done < <(jq -c '.[]' "$json_path")
+
